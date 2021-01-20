5aea28d10712dbd2fd12a173b60d03286f15b6c9
jgeng <jessiiee@gmail.com>
2021-01-20 18:43:44 -0500

    finished hamming exercise

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,10 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError('The two DNA strands must be of the same length.')
+    differences = 0
+    index = 0
+    for i in strand_a:
+        if i != strand_b[index]:
+            differences += 1
+        index += 1
+    return differences
