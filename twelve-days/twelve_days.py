5252dde47a0146c15f2c4d10b649cb9d6ebc1141
jgeng <jessiiee@gmail.com>
2021-01-21 15:56:33 -0500

    finished twelve_days exercise

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -1,2 +1,16 @@
+nth = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+
+gifts = ["a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens", "four Calling Birds", "five Gold Rings", "six Geese-a-Laying", "seven Swans-a-Swimming", "eight Maids-a-Milking", "nine Ladies Dancing", "ten Lords-a-Leaping", "eleven Pipers Piping", "twelve Drummers Drumming"]
+
 def recite(start_verse, end_verse):
-    pass
+    lines = []
+    for day in range(start_verse-1, end_verse):
+        lines.append(f"On the {nth[day]} day of Christmas my true love gave to me:") 
+        for gift in range(day, -1, -1):
+            if gift == 0 and day != 0:
+                lines[len(lines)-1] += f" and {gifts[gift]}."
+            elif gift == 0 and day == 0:
+                lines[len(lines)-1] += f" {gifts[gift]}."
+            else:
+                lines[len(lines)-1] += f" {gifts[gift]},"
+    return lines
