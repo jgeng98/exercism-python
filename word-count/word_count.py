6dada12a2c9c9cab9df675f54e2ae62aa4c7df10
jgeng <jessiiee@gmail.com>
2021-01-20 22:38:01 -0500

    finished word_count exercise

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,18 @@
+import string, regex
+
 def count_words(sentence):
-    pass
+    counts = dict()
+    words = regex.split(r'[_ ,\n\t]+', sentence.lower())
+    
+    
+    for word in words:
+        word = word.strip(string.punctuation)
+        if word in counts:
+            counts[word] += 1 
+        else:
+            counts[word] = 1
+    
+    if '' in counts.keys():
+        return {k: v for k, v in counts.items() if k is not ''}
+    else:
+        return counts
