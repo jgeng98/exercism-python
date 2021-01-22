61c87b588b14bc52e7e88247613b84fe53bd9762
jgeng <jessiiee@gmail.com>
2021-01-22 13:45:51 -0500

    finished acronym exercise

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,15 @@
+import string 
+
 def abbreviate(words):
-    pass
+    # delete apostrophe characters and replace all other punctuation with white space characters
+    words = words.translate(str.maketrans(string.punctuation,' '*len(string.punctuation), '\''))
+    
+    # split the sentence into words
+    words = words.split()
+    abb = ""
+
+    # concatenate the first letter of every word to the abbreviation
+    for word in words:
+        abb += word[0].upper()
+
+    return abb
