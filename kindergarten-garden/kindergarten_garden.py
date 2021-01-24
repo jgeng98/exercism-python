80acb0c12f389b3a758cb001ff6f31d5133dde60
jgeng <jessiiee@gmail.com>
2021-01-24 17:24:06 -0500

    finished kindergarten_garden exercise

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,21 @@
 class Garden:
-    def __init__(self, diagram, students):
-        pass
+    def __init__(self, diagram, students=None):
+        default_names = ["Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"]
+        self.students = sorted(sorted) if students is not None else default_names
+        self.diagram = diagram.split("\n")
+
+    def plants(self, name):
+        plants = []
+        for row in self.diagram:
+            index = self.students.index(name)*2
+            for num in range(index, index+2):
+                if row[num] == 'C':
+                    plants.append("Clover")
+                elif row[num] == 'G':
+                    plants.append("Grass")
+                elif row[num] == 'R':
+                    plants.append("Radishes")
+                elif row[num] == 'V':
+                    plants.append("Violets")
+        
+        return plants
