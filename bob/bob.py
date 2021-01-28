10b69267f2b63f518203bf9b2ad06ec9055bbcac
jgeng <jessiiee@gmail.com>
2021-01-28 00:45:21 -0500

    finished bob exercise

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,17 @@
 def response(hey_bob):
-    pass
+    # yelling a question at him
+    if hey_bob.isupper() and hey_bob.endswith('?'):
+        return "Calm down, I know what I'm doing!"
+    # yelling a statement at him
+    elif hey_bob.isupper() and not hey_bob.endswith('?'): 
+        return "Whoa, chill out!"
+    # address him without saying anything
+    # ie. empty string or only whitespace characters 
+    elif hey_bob.isspace() or not hey_bob:
+        return "Fine. Be that way!"
+    # ask him a question (trimming off whitespace)
+    elif hey_bob.strip().endswith('?'):
+        return "Sure."
+    # any other case 
+    else:
+        return "Whatever."
