6589aa03391c52103e4ee4294540226ac0be6d63
jgeng <jessiiee@gmail.com>
2021-01-28 00:06:58 -0500

    finished isbn-verifier exercise

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,2 +1,30 @@
+import regex 
+
 def is_valid(isbn):
-    pass
+    # verify that if a check digit exists, it must be in the correct spot
+    if 'X' in isbn and isbn[len(isbn)-1] != 'X':
+        return False
+    
+    # substitute everything that isn't a digit or 'X' with nothing
+    isbn = regex.sub(r'[^0-9X]', '', isbn)
+
+    # if the resulting string length isn't 10, immediately return false
+    if len(isbn) != 10:
+        return False
+
+    index = 0
+    sum = 0 
+
+    for digit in isbn:
+        # replace check digit with 10 in the sum
+        if digit == 'X':
+            index += 10
+            sum += index
+            continue
+        index += int(digit)
+        sum += index
+    
+    if sum % 11 == 0:
+        return True
+    else:
+        return False
