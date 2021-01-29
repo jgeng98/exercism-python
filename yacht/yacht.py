4364176ef5355a8ce38d003dd67bc58953e46f6a
jgeng <jessiiee@gmail.com>
2021-01-28 21:47:32 -0500

    finished yacht exercise

--- yacht/yacht.py
+++ yacht/yacht.py
@@ -10,23 +10,25 @@ those constants (ex. the constants in the os, subprocess and re modules).
 
 You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
 """
-
+from collections import Counter
 
 # Score categories.
 # Change the values as you see fit.
-YACHT = None
-ONES = None
-TWOS = None
-THREES = None
-FOURS = None
-FIVES = None
-SIXES = None
-FULL_HOUSE = None
-FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
-CHOICE = None
+YACHT = lambda dice : 50 if dice.count(dice[0]) == len(dice) else 0
+ONES = lambda dice : dice.count(1)
+TWOS = lambda dice : 2*dice.count(2)
+THREES = lambda dice : 3*dice.count(3)
+FOURS = lambda dice : 4*dice.count(4)
+FIVES = lambda dice : 5*dice.count(5)
+SIXES = lambda dice : 6*dice.count(6)
+FULL_HOUSE = lambda dice : sum(dice) if sorted(Counter(dice).values()) == [2, 3] else 0
+FOUR_OF_A_KIND = lambda dice : 4*next(key for key, value in Counter(dice).items() if value == 4 or value == 5) if 4 in Counter(dice).values() or 5 in Counter(dice).values() else 0
+LITTLE_STRAIGHT = lambda dice : 30 if dice == list(range(1, 6)) else 0
+BIG_STRAIGHT = lambda dice : 30 if dice == list(range(2, 7)) else 0
+CHOICE = lambda dice : sum(dice)
 
 
 def score(dice, category):
-    pass
+    dice.sort()
+
+    return category(dice)
